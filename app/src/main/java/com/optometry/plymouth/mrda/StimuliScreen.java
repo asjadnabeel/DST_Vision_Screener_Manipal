c7650ac105320389cf5d1edbd32eeb1486e62002
johnb <john.banez@students.plymouth.ac.uk>
2017-02-11 15:08:07 +0000

    added a comment

--- app/src/main/java/com/optometry/plymouth/mrda/StimuliScreen.java
+++ app/src/main/java/com/optometry/plymouth/mrda/StimuliScreen.java
@@ -20,7 +20,7 @@ public class StimuliScreen extends Activity {
     LinearLayout stimuliPanel;
     int circleAmount = 5;
     int level = 1;
-    //t
+    //ty
     int numOfRounds = 19;
 
     Bitmap[] levelList = new Bitmap[circleAmount];
